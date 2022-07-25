f8a2f2c370691a42c1d375ce4f0e6086023d5254
x1nigo <109367483+x1nigo@users.noreply.github.com>
2022-07-26 03:12:32 +0800

    Update config.h

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't us
 static const unsigned int alpha = 0xff;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Fira Mono:pixelsize=14:regular:antialias=true:autohint=true",
+	"Bitstream Vera Sans Mono:pixelsize=14:regular:antialias=true:autohint=true",
 	"NotoColorEmoji:pixelsize=14:regular:antialias=true:autohint=true"
 };
 static const char *prompt      = "RUN:";      /* -p  option; prompt to the left of input field */
