27dc54a2e77487019097e80ef55b10c8d3c29c2f
x1nigo <109367483+x1nigo@users.noreply.github.com>
2022-08-07 16:46:35 +0800

    Update config.h

--- config.h
+++ config.h
@@ -3,27 +3,28 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int colorprompt = 0;                /* -p  option; if 1, prompt uses SchemeSel, otherwise SchemeNorm */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching
+*/
 static const unsigned int alpha = 0xff;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Bitstream Vera Sans Mono:pixelsize=14:regular:antialias=true:autohint=true",
-	"NotoColorEmoji:pixelsize=14:regular:antialias=true:autohint=true"
+        "Bitstream Vera Sans Mono:pixelsize=14:regular:antialias=true:autohint=true",
+        "NotoColorEmoji:pixelsize=14:regular:antialias=true:autohint=true"
 };
 static const char *prompt      = "RUN:";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#b8dcff", "#121218" },
-	[SchemeSel] = { "#b8dcff", "#212338" },
-    [SchemeSelHighlight] = { "#00ffff", "#212338" },
-    [SchemeNormHighlight] = { "#ffc978", "#121218" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+        /*     fg         bg       */
+        [SchemeNorm] = { "#eee", "#000" },
+        [SchemeSel] = { "#eee", "#121228" },
+        [SchemeSelHighlight] = { "#5ebdab", "#121228" },
+        [SchemeNormHighlight] = { "#fea44c", "#000" },
+        [SchemeOut] = { "#000000", "#00ffff" },
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
-	[SchemeNorm] = { OPAQUE, alpha },
-	[SchemeSel] = { OPAQUE, alpha },
-	[SchemeOut] = { OPAQUE, alpha },
+        [SchemeNorm] = { OPAQUE, alpha },
+        [SchemeSel] = { OPAQUE, alpha },
+        [SchemeOut] = { OPAQUE, alpha },
 };
 
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
