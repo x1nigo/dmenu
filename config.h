c69f47aef42db8f92b40c744d2211c4b596e47da
x1nigo <109367483+x1nigo@users.noreply.github.com>
2022-12-16 00:07:28 +0800

    Update config.h

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static const unsigned int alpha = 0xf0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-        "Liberation Mono:pixelsize=14:regular:antialias=true:autohint=true",
+        "Fira Mono:pixelsize=14:regular:antialias=true:autohint=true",
         "NotoColorEmoji:pixelsize=12:regular:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
