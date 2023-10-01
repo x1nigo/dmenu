f9b953dc0a2099f96517e3ea78b0034043ec6e30
Chris Iñigo <109367483+x1nigo@users.noreply.github.com>
2023-10-02 02:56:36 +0800

    Add files via upload

---
+++ config.h
@@ -0,0 +1,31 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static const unsigned int alpha = 0xef;     /* Amount of opacity. 0xff is opaque             */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+        "monospace:size=10:regular:antialias=true:autohint=true",
+        "NotoColorEmoji:pixelsize=8:regular:antialias=true:autohint=true"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#ebdbb2", "#1d2021" },
+	[SchemeSel] = { "#ebdbb2", "#002a2c" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
