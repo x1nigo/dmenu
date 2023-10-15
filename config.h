c1b046dc241e8540a87405d4785ac9068b09f18c
Chris Iñigo <109367483+x1nigo@users.noreply.github.com>
2023-10-16 03:33:47 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -11,8 +11,8 @@ static const char *fonts[] = {
 static const char *prompt      = "Run:";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#eeeeee", "#18181b" },
-	[SchemeSel]  = { "#0a0f14", "#c0caf5" },
+	[SchemeNorm] = { "#eeeeee", "#1d2021" },
+	[SchemeSel]  = { "#eeeeee", "#005577" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 
