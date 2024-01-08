14323f2f65a86e760df6ceb555eba1beffe3fbd3
Chris Inigo <chris@x1nigo.xyz>
2024-01-09 01:39:09 +0800

    changed colors

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 0xff;     /* Amount of opacity. 0xff is opaque             */
+static const unsigned int alpha = 0xef;     /* Amount of opacity. 0xff is opaque             */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const int user_bh = 2;               /* add an defined amount of pixels to the bar height */
 
@@ -13,7 +13,7 @@ static const char *fonts[] = {
 static const char *prompt = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#d7d7d7", "#121618" },
+	[SchemeNorm] = { "#d7d7d7", "#1d2021" },
 	[SchemeSel]  = { "#d7d7d7", "#0f2837" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
 };
