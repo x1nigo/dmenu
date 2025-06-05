2d4531090de686f390a12485570f492b86c11381
x1nigo <109367483+x1nigo@users.noreply.github.com>
2025-06-05 15:08:04 +0000

    Add files via upload

--- config.h
+++ config.h
@@ -5,13 +5,13 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static const unsigned int alpha = 0xef;     /* Amount of opacity. 0xff is opaque             */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-		"monospace:size=10", "NotoColorEmoji:pixelsize=8", "FontAwesome:size=10"
+		"monospace:size=10", "NotoColorEmoji:pixelsize=8:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*               fg         bg       */
-	[SchemeNorm] = { "#ebdbb2", "#121618" },
-	[SchemeSel]  = { "#ff2828", "#870000" },
+	[SchemeNorm] = { "#ebdbb2", "#1d2021" },
+	[SchemeSel]  = { "#ebdbb2", "#005577" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 
