ea497c8d95acce66e1d41f22407a7db0491cb5db
x1nigo <109367483+x1nigo@users.noreply.github.com>
2025-09-13 01:10:28 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 0xef;     /* Amount of opacity. 0xff is opaque             */
+static const unsigned int alpha = 0xff;     /* Amount of opacity. 0xff is opaque             */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 		"monospace:size=10", "NotoColorEmoji:pixelsize=8:antialias=true:autohint=true"
@@ -11,17 +11,20 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*               fg         bg       */
 	[SchemeNorm] = { "#ebdbb2", "#1d2021" },
-	[SchemeSel]  = { "#ebdbb2", "#005577" },
+	[SchemeSel]  = { "#282828", "#005577" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
 	[SchemeNorm] = { OPAQUE, alpha },
-	[SchemeSel] = { OPAQUE, alpha },
-	[SchemeOut] = { OPAQUE, alpha },
+	[SchemeSel]  = { OPAQUE, alpha },
+	[SchemeOut]  = { OPAQUE, alpha },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 0;
+static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words
